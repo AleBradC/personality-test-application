26d7eff452b57d2516f39a77aa49c4e5471ae3e3
AleBradC <alexandrabradclaudia@gmail.com>
2024-02-25 15:33:13 +0200

    setup and connection to db

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import bodyParser from "body-parser";
+import { connectDB } from "../dbConnection";
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.DB_URI as string;
 
 const app = express();
 
 // middleswares
+app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
 
@@ -12,6 +20,8 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello");
 });
 
-app.listen(8000, () => {
-  console.log(`App is running on port ${8000}`);
+connectDB(MONGO_URI);
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
