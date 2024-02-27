8ce594091f4a91310b3f6b4ba9d69f542c259a83
AleBradC <alexandrabradclaudia@gmail.com>
2024-02-27 16:06:24 +0200

    clean up

--- server/src/tests/unit/routes/answer.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../../../app";
-import { STATUS_CODE } from "../../../utils/constants";
-
-describe("answer route", () => {
-  it("should get the data on /api/answer", async () => {
-    return request(app).get("/api/answer").expect(STATUS_CODE.OK);
-  });
-});
