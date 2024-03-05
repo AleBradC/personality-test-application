f2e7518f8561aaadb8f1da1cfd1a70754da4720d
AleBradC <alexandrabradclaudia@gmail.com>
2024-03-05 20:54:58 +0200

    added tests

--- server/src/repositories/QuestionRepository.ts
+++ server/src/repositories/QuestionRepository.ts
@@ -11,7 +11,7 @@ import { STATUS_CODE } from "../utils/constants";
 export default class QuestionRepository implements IQuestionRepositoryLayer {
   getQuestions = async (): Promise<IQuestion[]> => {
     try {
-      return await Question.find({}, { _id: 0, __v: 0 }).lean();
+      return await Question.find({}, { _id: 0, __v: 0 });
     } catch (error) {
       throw new CustomError(STATUS_CODE.BAD_REQUEST, (error as Error).message);
     }
